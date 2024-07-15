2cff7fba380310767be3269852b0a0f8df4562fd
Manjur-WD <mak985812@gmail.com>
2024-07-16 00:41:01 +0530

    updated

--- js/gsap-custom.js
+++ js/gsap-custom.js
@@ -1,1 +1,31 @@
+// OUR STORE
+Splitting();
+const ourStoreTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#our-store-wrapper",
+        start: "top 50%",
+        end: "50% 50%",
+        // markers: true,
+        scrub: 1,
+        yoyo: true,
+    }
+});
 
+ourStoreTl.from("#our-store-wrapper .subhead .char",{
+    y:50,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.05,
+    ease: "back.out"
+})
+
+ourStoreTl.from(".locator", {
+    opacity: 0,
+    duration: 1,
+    stagger: 0.5,
+    ease: "back.out"
+})
+ourStoreTl.from(".connectors", {
+    opacity: 0,
+    duration: 1,
+})
