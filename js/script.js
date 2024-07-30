e96e9aaf76304e4be435952523e76122f6e27cb4
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2024-07-31 00:42:49 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -73,6 +73,44 @@ $(".banner-slide-nav #next").click(() => {
   $(".slick-next").click();
 })
 
+$('.partners-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 2000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 $('.brands-slider').slick({
   dots: false,
   infinite: true,
